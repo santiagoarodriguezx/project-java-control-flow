3b9ce5f3b13f8258c4463b9e4ff762e3f5575466
Santiago Alfonso Rodriguez <santiago_alfonsoro@soy.sena.edu.co>
2025-05-09 23:51:23 -0500

    Desafio FizzBuzz mejorado

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa el inicio del rango: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Ingresa el final del rango: ");
+        int end = scanner.nextInt();
+
+        for (int i = start; i <= end; i++) {
+            if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+                System.out.println("FizzBuzzBoom");
+            } else if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
+            } else if (i % 7 == 0) {
+                System.out.println("Boom");
             } else {
                 System.out.println(i);
             }
         }
+
+        scanner.close();
     }
 }
