21ff98a86b87b30117a90cee8e2f8ef70bfd7291
Santiago Alfonso Rodriguez <santiago_alfonsoro@soy.sena.edu.co>
2025-05-09 23:42:18 -0500

    Combinando Estructuras de Control en Java

---
+++ PrimeNumberFinder.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrimeNumberFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int number = 2;
+
+        System.out.print("How many prime numbers do you want to find? ");
+        int maxPrimes = scanner.nextInt();
+
+        System.out.println("First " + maxPrimes + " prime numbers:");
+
+        while (count < maxPrimes) {
+            boolean isPrime = true;
+        
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        
+            if (isPrime) {
+                System.out.print(number + " ");
+                count++;
+            }
+        
+            number++;
+        }
+
+        scanner.close();
+    }
+}
